5a08b56aa11b3fc4f6fb12677b6b6a220a54dafb
izardy <49428004+izardy@users.noreply.github.com>
2023-09-17 11:22:37 +0800

    update

--- etl/ingestion/infoCaptain.py
+++ etl/ingestion/infoCaptain.py
@@ -2,14 +2,14 @@ import pandas as pd
 import numpy as np
 import os
 
-infoCaptain=pd.read_csv('../data/captain_schedule/processed/infoCAPTAIN.csv')
+infoCaptain=pd.read_csv('/home/hadoop/MyRapidHack2023-BackEnd/data/captain_schedule/processed/infoCAPTAIN.csv')
 infoCaptain['cs_date']=pd.to_datetime(infoCaptain['cs_date'])
 infoCaptain['ope_date']=pd.to_datetime(infoCaptain['ope_date'])
 infoCaptain=infoCaptain.rename(columns={'dt_gps_max':'dt_endduty','dt_gps_min':'dt_startduty'})
 
 infoCaptain=infoCaptain.to_json(orient = "records")
 
-with open('../data/processed/json/infoCaptain.json', 'w') as f:
+with open('/home/hadoop/MyRapidHack2023-BackEnd/data/processed/json/infoCaptain.json', 'w') as f:
     f.write(infoCaptain)
 
 # run once to create solr collection infoCaptain /home/hadoop/solr-9.3.0/bin/solr create -c infoCaptain
