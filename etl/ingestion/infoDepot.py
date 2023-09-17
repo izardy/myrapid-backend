984d52952acf23c2f7d908186cb17dea43b29030
izardy <izardyamir@gmail.com>
2023-09-17 11:05:40 +0800

    modified:   infoDepot.py

--- etl/ingestion/infoDepot.py
+++ etl/ingestion/infoDepot.py
@@ -2,14 +2,14 @@ import pandas as pd
 import numpy as np
 import os
 
-infoDepot=pd.read_csv('../data/processed/InfoDEPOT.csv')
+infoDepot=pd.read_csv('/home/hadoop/MyRapidHack2023-BackEnd/data/processed/InfoDEPOT.csv')
 infoDepot=infoDepot.to_json(orient = "records")
 
-with open('../data/processed/json/infoDepot.json', 'w') as f:
+with open('/home/hadoop/MyRapidHack2023-BackEnd/data/processed/json/infoDEPOT.json', 'w') as f:
     f.write(infoDepot)
 
-# run once to create solr collection infoDepot /home/hadoop/solr-9.3.0/bin/solr create -c infoDepot
-os.system('/home/hadoop/solr-9.3.0/bin/solr create -c infoDepot')
+# run once to create solr collection infoDepot /home/hadoop/solr-9.3.0/bin/solr create -c infoDEPOT
+# os.system('/home/hadoop/solr-9.3.0/bin/solr create -c infoDEPOT')
 
-# run once to post data into infoDepot collection /home/hadoop/solr-9.3.0/bin/post -c infoDepot /home/hadoop/MyRapidHack2023-BackEnd/data/processed/json/infoDepot.json
-os.system('/home/hadoop/solr-9.3.0/bin/post -c infoDepot /home/hadoop/MyRapidHack2023-BackEnd/data/processed/json/infoDepot.json')
+# run once to post data into infoDepot collection /home/hadoop/solr-9.3.0/bin/post -c infoDEPOT /home/hadoop/MyRapidHack2023-BackEnd/data/processed/json/infoDEPOT.json
+# os.system('/home/hadoop/solr-9.3.0/bin/post -c infoDEPOT /home/hadoop/MyRapidHack2023-BackEnd/data/processed/json/infoDEPOT.json')
