69fd3743eee463e50b95027dce51bc9f2590c6ff
izardy <49428004+izardy@users.noreply.github.com>
2023-09-17 22:25:11 +0800

    update

--- etl/ingestion/infoDepot.py
+++ etl/ingestion/infoDepot.py
@@ -2,7 +2,7 @@ import pandas as pd
 import geopandas as gpd
 import numpy as np
 import os
-'''
+
 infoDepot=pd.read_csv('/home/hadoop/MyRapidHack2023-BackEnd/data/processed/InfoDEPOT.csv')
 
 infoDepot['depot_name']=infoDepot['depot_name'].str.strip()
@@ -15,16 +15,16 @@ infoDepot['stop_name']=infoDepot['stop_name'].str.strip()
 infoDepot['street_name']=infoDepot['street_name'].str.strip()
 
 #infoDepot['geolocation']=gpd.points_from_xy(infoDepot['latitude'], infoDepot['longitude'])
-gdf = gpd.GeoDataFrame(infoDepot, geometry=gpd.points_from_xy(infoDepot.latitude, infoDepot.longitude)) 
+#gdf = gpd.GeoDataFrame(infoDepot, geometry=gpd.points_from_xy(infoDepot.latitude, infoDepot.longitude)) 
 
-gdf.to_file("/home/hadoop/MyRapidHack2023-BackEnd/data/processed/json/infoDepot.json", driver="GeoJSON")
-#infoDepot=infoDepot.to_json(orient = "records")
+#gdf.to_file("/home/hadoop/MyRapidHack2023-BackEnd/data/processed/json/infoDepot.json", driver="GeoJSON")
+infoDepot=infoDepot.to_json(orient = "records")
 
-#with open('/home/hadoop/MyRapidHack2023-BackEnd/data/processed/json/infoDEPOT.json', 'w') as f:
- #   f.write(infoDepot)
+with open('/home/hadoop/MyRapidHack2023-BackEnd/data/processed/json/infoDEPOT.json', 'w') as f:
+    f.write(infoDepot)
 
 # run once to create solr collection infoDepot /home/hadoop/solr-9.3.0/bin/solr create -c infoDEPOT
 #os.system('/home/hadoop/solr-9.3.0/bin/solr create -c infoDEPOT')
-'''
+
 # run once to post data into infoDepot collection /home/hadoop/solr-9.3.0/bin/post -c infoDEPOT /home/hadoop/MyRapidHack2023-BackEnd/data/processed/json/infoDEPOT.json
 os.system('/home/hadoop/solr-9.3.0/bin/post -c infoDEPOT /home/hadoop/MyRapidHack2023-BackEnd/data/processed/json/infoDEPOT.json')
