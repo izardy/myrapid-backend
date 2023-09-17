bbdd9741b62743b432d252e1577a00d0150b3302
izardy <49428004+izardy@users.noreply.github.com>
2023-09-17 11:26:01 +0800

    update

--- etl/ingestion/infoCaptain.py
+++ etl/ingestion/infoCaptain.py
@@ -9,11 +9,11 @@ infoCaptain=infoCaptain.rename(columns={'dt_gps_max':'dt_endduty','dt_gps_min':'
 
 infoCaptain=infoCaptain.to_json(orient = "records")
 
-with open('/home/hadoop/MyRapidHack2023-BackEnd/data/processed/json/infoCaptain.json', 'w') as f:
+with open('/home/hadoop/MyRapidHack2023-BackEnd/data/processed/json/infoCAPTAIN.json', 'w') as f:
     f.write(infoCaptain)
 
 # run once to create solr collection infoCaptain /home/hadoop/solr-9.3.0/bin/solr create -c infoCaptain
-os.system('/home/hadoop/solr-9.3.0/bin/solr create -c infoCaptain')
+os.system('/home/hadoop/solr-9.3.0/bin/solr create -c infoCAPTAIN')
 
 # run once to post data into infoCaptain collection /home/hadoop/solr-9.3.0/bin/post -c infoCaptain /home/hadoop/MyRapidHack2023-BackEnd/data/processed/json/infoCaptain.json
-os.system('/home/hadoop/solr-9.3.0/bin/post -c infoCaptain /home/hadoop/MyRapidHack2023-BackEnd/data/processed/json/infoCaptain.json')
+os.system('/home/hadoop/solr-9.3.0/bin/post -c infoCAPTAIN /home/hadoop/MyRapidHack2023-BackEnd/data/processed/json/infoCAPTAIN.json')
