2db120472439137510336a1b7d3fc0a75d52ce24
izardy <49428004+izardy@users.noreply.github.com>
2023-09-18 00:22:03 +0800

    update

--- etl/ingestion/infoDepot.py
+++ etl/ingestion/infoDepot.py
@@ -26,7 +26,7 @@ with open('/home/hadoop/MyRapidHack2023-BackEnd/data/processed/json/infoDEPOT.js
     f.write(infoDepot)
 
 # run once to create solr collection infoDepot /home/hadoop/solr-9.3.0/bin/solr create -c infoDEPOT
-os.system('/home/hadoop/solr-9.3.0/bin/solr create -c infoDEPOT')
+# os.system('/home/hadoop/solr-9.3.0/bin/solr create -c infoDEPOT')
 
 # run once to post data into infoDepot collection /home/hadoop/solr-9.3.0/bin/post -c infoDEPOT /home/hadoop/MyRapidHack2023-BackEnd/data/processed/json/infoDEPOT.json
 os.system('/home/hadoop/solr-9.3.0/bin/post -c infoDEPOT /home/hadoop/MyRapidHack2023-BackEnd/data/processed/json/infoDEPOT.json')
