3962375d575f3d371fe0d7ab64314f6070929968
izardy <49428004+izardy@users.noreply.github.com>
2023-09-18 23:16:16 +0800

    update

--- etl/ingestion/infoCaptain.py
+++ etl/ingestion/infoCaptain.py
@@ -3,8 +3,8 @@ import numpy as np
 import os
 
 infoCaptain=pd.read_csv('/home/hadoop/MyRapidHack2023-BackEnd/data/captain_schedule/processed/infoCAPTAIN.csv')
-infoCaptain['cs_date']=pd.to_datetime(infoCaptain['cs_date'])
-infoCaptain['ope_date']=pd.to_datetime(infoCaptain['ope_date'])
+infoCaptain['cs_date']=pd.to_datetime(infoCaptain['cs_date']+ ' 00:00:00').dt.strftime('%Y-%m-%d %H:%M:%S')
+infoCaptain['ope_date']=pd.to_datetime(infoCaptain['ope_date']+ ' 00:00:00').dt.strftime('%Y-%m-%d %H:%M:%S')
 infoCaptain=infoCaptain.rename(columns={'dt_gps_max':'dt_endduty','dt_gps_min':'dt_startduty'})
 
 infoCaptain=infoCaptain.to_json(orient = "records")
@@ -13,7 +13,7 @@ with open('/home/hadoop/MyRapidHack2023-BackEnd/data/processed/json/infoCAPTAIN.
     f.write(infoCaptain)
 
 # run once to create solr collection infoCaptain /home/hadoop/solr-9.3.0/bin/solr create -c infoCaptain
-os.system('/home/hadoop/solr-9.3.0/bin/solr create -c infoCAPTAIN')
+# os.system('/home/hadoop/solr-9.3.0/bin/solr create -c infoCAPTAIN')
 
 # run once to post data into infoCaptain collection /home/hadoop/solr-9.3.0/bin/post -c infoCaptain /home/hadoop/MyRapidHack2023-BackEnd/data/processed/json/infoCaptain.json
 os.system('/home/hadoop/solr-9.3.0/bin/post -c infoCAPTAIN /home/hadoop/MyRapidHack2023-BackEnd/data/processed/json/infoCAPTAIN.json')
